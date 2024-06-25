b9e9dbf6e442f385779510054c937d3ed7cbd198
fgmomo <konemohamedflo@gmail.com>
2024-06-25 15:21:01 +0000

    final 2

--- Base de données/ticket.sql
+++ Base de données/ticket.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : lun. 24 juin 2024 à 18:18
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.2.12
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 25 juin 2024 à 12:36
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.2.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,8 +27,10 @@ SET time_zone = "+00:00";
 -- Structure de la table `admin`
 --
 
-CREATE TABLE `admin` (
-  `id` bigint(20) NOT NULL
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` bigint NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -37,8 +39,10 @@ CREATE TABLE `admin` (
 -- Structure de la table `apprenant`
 --
 
-CREATE TABLE `apprenant` (
-  `id` bigint(20) NOT NULL
+DROP TABLE IF EXISTS `apprenant`;
+CREATE TABLE IF NOT EXISTS `apprenant` (
+  `id` bigint NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -47,13 +51,15 @@ CREATE TABLE `apprenant` (
 -- Structure de la table `base_connaissance`
 --
 
-CREATE TABLE `base_connaissance` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `base_connaissance`;
+CREATE TABLE IF NOT EXISTS `base_connaissance` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `date_update` datetime(6) DEFAULT NULL,
-  `lien` varchar(255) DEFAULT NULL,
-  `piece_jointe` varchar(255) DEFAULT NULL,
-  `question` varchar(255) NOT NULL,
-  `reponse` tinytext NOT NULL
+  `lien` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `piece_jointe` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `question` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `reponse` tinytext COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -62,11 +68,13 @@ CREATE TABLE `base_connaissance` (
 -- Structure de la table `categorie`
 --
 
-CREATE TABLE `categorie` (
-  `id` bigint(20) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `libelle` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `libelle` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `categorie`
@@ -83,11 +91,13 @@ INSERT INTO `categorie` (`id`, `description`, `libelle`) VALUES
 -- Structure de la table `etat`
 --
 
-CREATE TABLE `etat` (
-  `id` bigint(20) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `libelle` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `etat`;
+CREATE TABLE IF NOT EXISTS `etat` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `libelle` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `etat`
@@ -104,8 +114,10 @@ INSERT INTO `etat` (`id`, `description`, `libelle`) VALUES
 -- Structure de la table `formateur`
 --
 
-CREATE TABLE `formateur` (
-  `id` bigint(20) NOT NULL
+DROP TABLE IF EXISTS `formateur`;
+CREATE TABLE IF NOT EXISTS `formateur` (
+  `id` bigint NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -114,11 +126,13 @@ CREATE TABLE `formateur` (
 -- Structure de la table `priorite`
 --
 
-CREATE TABLE `priorite` (
-  `id` bigint(20) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `libelle` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `priorite`;
+CREATE TABLE IF NOT EXISTS `priorite` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `libelle` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `priorite`
@@ -135,13 +149,17 @@ INSERT INTO `priorite` (`id`, `description`, `libelle`) VALUES
 -- Structure de la table `reponse`
 --
 
-CREATE TABLE `reponse` (
-  `id` bigint(20) NOT NULL,
-  `contenu` varchar(255) NOT NULL,
+DROP TABLE IF EXISTS `reponse`;
+CREATE TABLE IF NOT EXISTS `reponse` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `contenu` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `date_creation` datetime(6) NOT NULL,
-  `auteur_id` bigint(20) DEFAULT NULL,
-  `ticket_id` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `auteur_id` bigint DEFAULT NULL,
+  `ticket_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKa7qgb8ifj7esqqkm7i21gf3qt` (`auteur_id`),
+  KEY `FKjyirw9foyfoktillhnecbiidn` (`ticket_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `reponse`
@@ -155,7 +173,15 @@ INSERT INTO `reponse` (`id`, `contenu`, `date_creation`, `auteur_id`, `ticket_id
 (5, 'Essaye de redemarer le PC !!!', '2024-06-24 16:11:39.000000', 7, 1),
 (6, 'Essaye de redemarer le PC !!!', '2024-06-24 16:11:39.000000', 7, 1),
 (7, 'Apporte le dans la salle de maintenance !!!', '2024-06-24 16:15:42.000000', 7, 2),
-(8, 'Apporte le dans la salle de maintenance !!!', '2024-06-24 16:15:42.000000', 7, 2);
+(8, 'Apporte le dans la salle de maintenance !!!', '2024-06-24 16:15:42.000000', 7, 2),
+(9, 'IMPOSSIBLE.....', '2024-06-24 23:48:57.290200', 8, 2),
+(10, 'IMPOSSIBLE.....', '2024-06-24 23:48:57.290200', 8, 2),
+(11, 'IMPOSSIBLE.....', '2024-06-25 00:09:10.819335', 8, 3),
+(12, 'IMPOSSIBLE.....', '2024-06-25 00:09:10.819335', 8, 3),
+(13, 'AUCUNE IDEE Monsieur va réviser..', '2024-06-25 08:33:13.255046', 7, 4),
+(14, 'AUCUNE IDEE Monsieur va réviser..', '2024-06-25 08:33:13.255046', 7, 4),
+(15, 'AUCUNE IDEE  ..', '2024-06-25 12:23:58.026737', 3, 7),
+(16, 'BBBBRHERTUH5RUJ', '2024-06-25 12:34:19.882294', 3, 8);
 
 -- --------------------------------------------------------
 
@@ -163,10 +189,12 @@ INSERT INTO `reponse` (`id`, `contenu`, `date_creation`, `auteur_id`, `ticket_id
 -- Structure de la table `role`
 --
 
-CREATE TABLE `role` (
-  `id` bigint(20) NOT NULL,
-  `libelle` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `role`
@@ -185,18 +213,26 @@ INSERT INTO `role` (`id`, `libelle`) VALUES
 -- Structure de la table `ticket`
 --
 
-CREATE TABLE `ticket` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `ticket`;
+CREATE TABLE IF NOT EXISTS `ticket` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `date_creation` datetime(6) NOT NULL,
   `date_resolution` datetime(6) DEFAULT NULL,
-  `description` varchar(255) NOT NULL,
-  `apprenant_id` bigint(20) DEFAULT NULL,
-  `categorie_id` bigint(20) DEFAULT NULL,
-  `etat_id` bigint(20) DEFAULT NULL,
-  `formateur_id` bigint(20) DEFAULT NULL,
-  `priorite_id` bigint(20) DEFAULT NULL,
-  `reponse_formateur_id` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `description` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `apprenant_id` bigint DEFAULT NULL,
+  `categorie_id` bigint DEFAULT NULL,
+  `etat_id` bigint DEFAULT NULL,
+  `formateur_id` bigint DEFAULT NULL,
+  `priorite_id` bigint DEFAULT NULL,
+  `reponse_formateur_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK5rf0uqtn7xykjwek3fa0k1kl1` (`apprenant_id`),
+  KEY `FKtbdefmamq7yt6upk0e3odvioa` (`categorie_id`),
+  KEY `FK20jkfcs0ntha9831ancysgsmh` (`etat_id`),
+  KEY `FKksg09j87614naih0xwa78hre` (`formateur_id`),
+  KEY `FK4es3wncbq9bmlhdaqeuv1v67p` (`priorite_id`),
+  KEY `FKkwlpogf5128exg3l7v5i3nqpg` (`reponse_formateur_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `ticket`
@@ -204,7 +240,13 @@ CREATE TABLE `ticket` (
 
 INSERT INTO `ticket` (`id`, `date_creation`, `date_resolution`, `description`, `apprenant_id`, `categorie_id`, `etat_id`, `formateur_id`, `priorite_id`, `reponse_formateur_id`) VALUES
 (1, '2024-06-24 11:45:58.000000', '2024-06-24 16:11:39.000000', 'Mon Server Xamp demarre pas !!!', 1, 1, 3, 7, 1, 5),
-(2, '2024-06-24 16:13:51.000000', '2024-06-24 16:15:42.000000', 'Mon PC DEMARRE PASS !!!', 2, 1, 3, 8, 1, 7);
+(2, '2024-06-24 16:13:51.000000', '2024-06-24 23:48:57.290200', 'Mon PC DEMARRE PASS !!!', 2, 1, 3, 8, 1, 9),
+(3, '2024-06-25 00:07:19.078023', '2024-06-25 00:09:10.819335', 'essai last', 1, 3, 3, 8, 1, 11),
+(4, '2024-06-25 08:23:26.646491', '2024-06-25 08:33:13.255046', 'comment déclarer une variable de type liste en Scala', 11, 2, 3, 7, 3, 13),
+(5, '2024-06-25 09:34:57.841476', NULL, 'comment déclarer une variable', 11, 2, 1, NULL, 3, NULL),
+(6, '2024-06-25 09:35:29.377483', NULL, 'comment déclarer une variable', 11, 2, 1, NULL, 3, NULL),
+(7, '2024-06-25 11:36:00.628274', '2024-06-25 12:23:58.571780', 'j\'abandonne', 11, 2, 3, 3, 3, 15),
+(8, '2024-06-25 12:31:25.869885', '2024-06-25 12:34:19.928295', 'j\'abandonne b', 11, 2, 3, 3, 3, 16);
 
 -- --------------------------------------------------------
 
@@ -212,14 +254,18 @@ INSERT INTO `ticket` (`id`, `date_creation`, `date_resolution`, `description`, `
 -- Structure de la table `utilisateur`
 --
 
-CREATE TABLE `utilisateur` (
-  `id` bigint(20) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `motdepasse` varchar(255) DEFAULT NULL,
-  `nom` varchar(255) DEFAULT NULL,
-  `prenom` varchar(255) DEFAULT NULL,
-  `role_id` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `motdepasse` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `prenom` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `role_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKrma38wvnqfaf66vvmi57c71lo` (`email`),
+  KEY `FKaqe8xtajee4k0wlqrvh2pso4l` (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `utilisateur`
@@ -234,139 +280,8 @@ INSERT INTO `utilisateur` (`id`, `email`, `motdepasse`, `nom`, `prenom`, `role_i
 (6, 'admin3@gmail.com', '$2a$10$qLLrrjQNYByP4ga0ec4uT.SDSDeFRvYnwSe.ovJqMfhrbIAEa5bGy', 'Admin', 'Three', 3),
 (7, 'formatricekonate@gmail.com', '$2a$10$ql.oijgqooXNMaH4AMGdfuFYGEfyv8LJMfJfTCHkGUVaFOIAhb9yq', 'Formatrice', 'Konate', 2),
 (8, 'formatriceSissoko@gmail.com', '$2a$10$Q6b6wX.ChYXNoxi8iRuw.uo9qdosMv/vhJ4FDsjCTFxqbJ1HnRcF.', 'Formatrice', 'Sissoko', 2),
-(10, 'fakoro@gmail.com', '$2a$10$ThtDapSM32LkudoqCFGBjesniCwEfcHKb4weOklpQX//mTG.LpIf.', 'Fakoro', 'Traore', 1);
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `apprenant`
---
-ALTER TABLE `apprenant`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `base_connaissance`
---
-ALTER TABLE `base_connaissance`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `etat`
---
-ALTER TABLE `etat`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `formateur`
---
-ALTER TABLE `formateur`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `priorite`
---
-ALTER TABLE `priorite`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `reponse`
---
-ALTER TABLE `reponse`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FKa7qgb8ifj7esqqkm7i21gf3qt` (`auteur_id`),
-  ADD KEY `FKjyirw9foyfoktillhnecbiidn` (`ticket_id`);
-
---
--- Index pour la table `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `ticket`
---
-ALTER TABLE `ticket`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK5rf0uqtn7xykjwek3fa0k1kl1` (`apprenant_id`),
-  ADD KEY `FKtbdefmamq7yt6upk0e3odvioa` (`categorie_id`),
-  ADD KEY `FK20jkfcs0ntha9831ancysgsmh` (`etat_id`),
-  ADD KEY `FKksg09j87614naih0xwa78hre` (`formateur_id`),
-  ADD KEY `FK4es3wncbq9bmlhdaqeuv1v67p` (`priorite_id`),
-  ADD KEY `FKkwlpogf5128exg3l7v5i3nqpg` (`reponse_formateur_id`);
-
---
--- Index pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UKrma38wvnqfaf66vvmi57c71lo` (`email`),
-  ADD KEY `FKaqe8xtajee4k0wlqrvh2pso4l` (`role_id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `base_connaissance`
---
-ALTER TABLE `base_connaissance`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `categorie`
---
-ALTER TABLE `categorie`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `etat`
---
-ALTER TABLE `etat`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `priorite`
---
-ALTER TABLE `priorite`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `reponse`
---
-ALTER TABLE `reponse`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT pour la table `role`
---
-ALTER TABLE `role`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT pour la table `ticket`
---
-ALTER TABLE `ticket`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+(10, 'fakoro@gmail.com', '$2a$10$ThtDapSM32LkudoqCFGBjesniCwEfcHKb4weOklpQX//mTG.LpIf.', 'Fakoro', 'Traore', 1),
+(11, 'Mamoutou@gmail.com', '$2a$10$9feRP.hqxIvHdWGrUEcip.SN7VWq2UPgXOM4Ncg3OFgjslonOUCqS', 'Yalcouyé', 'Mamoutou', 1);
 
 --
 -- Contraintes pour les tables déchargées
